7b0e64e85ad6a9a7099dc464d417920258152307
Agustin <agustinmaldonado20@gmail.com>
2023-06-25 21:56:11 -0300

    Agregando slider

--- src/components/sobreNosotros/SobreNosotrosHeroe.jsx
+++ src/components/sobreNosotros/SobreNosotrosHeroe.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import slider1 from './slider1.jpg';
+import slider2 from './slider2.jpg';
+import slider3 from './slider3.jpg';
+import slider4 from './slider4.jpg';
+
 
 const SobreNosotrosHeroe = () => {
   return (
-    <section className='sobrenosotros__container'>
-        <h2>Quiénes Somos</h2>
-    </section>
+    <>
+    {/* <section className='sobrenosotros__container'>
+      <h2>Quiénes Somos</h2>
+    </section> */}
+    <div className='slider-frame sobrenosotros__container'>
+    <h2>Quiénes Somos</h2> 
+    <ul>
+          <li><img src={slider1} alt="img1" /></li>
+          <li><img src={slider2} alt="img2" /></li>
+          <li><img src={slider3} alt="img3" /></li>
+          <li><img src={slider4} alt="img4" /></li>
+        </ul>
+      </div>
+    </>
   )
 }
 
